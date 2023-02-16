7aac6ae4350dbe5829921fa6d0540b842f596461
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-02-16 04:56:38 +0100

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main - A program that prints a string with puts
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
