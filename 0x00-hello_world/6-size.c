887e52276a6a214456ebe6913f16ff252bab3680
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-02-16 05:56:39 +0100

    FIX: Betty style

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /*
- * main - A program that prints the size of various types on the computer it is compiled and run on.
+ * main - A program that prints the size of various types on the computer
+ * 		it is compiled and run on.
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
@@ -14,5 +14,5 @@ int main(void)
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
-	return 0;
+	return (0);
 }
