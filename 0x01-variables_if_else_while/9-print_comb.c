0d7fd9881a5a17eec53c4022dce0245f25817fc1
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-02-17 06:57:06 +0100

    9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints all possible
+ * combinations of single-digit numbers.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+
+	for (int i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+
+		char *comma = (i < 9) ? ", " : "\n";
+
+		while (*comma)
+			putchar(*(comma++));
+	}
+
+	return (0);
+}
