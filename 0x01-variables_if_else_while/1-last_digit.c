7d2dfa07760537e983602280f7c29d71f5ab65c9
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-02-17 10:36:52 +0100

    Updating handle_connection function

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,20 +18,15 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (!n)
-		printf("%d is zero\n", n);
-	else
-	{
-		int last_digit = n % 10;
+	int last_digit = n % 10;
 
-		char *sign = (n % 10 > 5) ? "and is greater than 5"
-				: "and is less than 6";
+	if (last_digit > 5)
+		printf("Last digit of %i is %i and is greater than 5\n", n, last_digit);
+	else if (last_digit == 0)
+		printf("Last digit of %i is %i and is 0\n", n, last_digit);
+	else if (last_digit < 6 && last_digit != 0)
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n,
+			last_digit);
 
-		char *is_zero = (n % 10 == 0) ? "and is 0" : "and not 0";
-
-		printf("Last digit of %d is %d %s %s\n", n, last_digit,
-		(last_digit) ? sign : "", (last_digit < 6) ? is_zero : "");
-
-	}
 	return (0);
 }
