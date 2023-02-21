f6d0fb9cdb4bc5d2bd7893484c141c83be4fe3ad
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-02-21 06:15:42 +0100

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ *
+ * return - void
+*/
+void	print_alphabet(void)
+{
+	char	c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
