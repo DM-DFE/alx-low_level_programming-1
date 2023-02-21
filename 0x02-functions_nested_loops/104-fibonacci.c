56b260300e1cda5cebdbc456d344691eef5627a0
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-02-21 13:23:23 +0100

    TEST: Testing an algo using strings

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,17 +5,53 @@
  *
  * Return: 0
 */
-int main(void)
-{
-	int i;
-	unsigned long prev = 1, curr = 1, next;
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
 
-	for (i = 0; i < 98; i++)
-	{
-		printf("%lu%s", curr, i == 97 ? "\n": ", ");
-		next = prev + curr;
-		prev = curr;
-		curr = next;
-	}
-	return (0);
+char *str_revese_inplace(char *s) {
+  char *left = s;
+  char *right = s + strlen(s);
+  char t;
+
+  while (right > left) {
+    right--;
+    t = *right;
+    *right = *left;
+    *left = t;
+    left++;
+  }
+  return s;
+}
+
+char *str_add(char *ssum, const char *sa, const char *sb) {
+  const char *pa = sa + strlen(sa);
+  const char *pb = sb + strlen(sb);
+  char *psum = ssum;
+  int carry = 0;
+  int sum;
+
+  while (pa > sa || pb > sb || carry) {
+    sum = carry;
+    if (pa > sa) sum += *(--pa) - '0';
+    if (pb > sb) sum += *(--pb) - '0';
+    *psum++ = sum % 10 + '0';
+    carry = sum / 10;
+  }
+  *psum = '\0';
+  return str_revese_inplace(ssum);
+}
+
+int main(void) {
+  char fib[3][300];
+  int i;
+
+  strcpy(fib[0], "0");
+  strcpy(fib[1], "1");
+  for (i = 2; i <= 98; i++) {
+    printf("%s%s", str_add(fib[2], fib[1], fib[0]), i < 98 ? ", " : "\n");
+    strcpy(fib[0], fib[1]);
+    strcpy(fib[1], fib[2]);
+  }
+  return 0;
 }
