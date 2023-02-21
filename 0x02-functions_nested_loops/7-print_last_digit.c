36714f7fca3d0ab2ac44716ce06bde878ab7a7ef
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-02-21 06:14:13 +0100

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+/**
+ * print_last_digit - prints the last digit of a number
+ *
+ * @n: number to be checked
+ *
+ * Return: last digit of n
+*/
+int	print_last_digit(int n)
+{
+	return (_putchar('0' + ABS(n % 10)));
+}
