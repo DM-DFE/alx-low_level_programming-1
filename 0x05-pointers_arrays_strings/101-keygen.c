3f03e48d9f5f7f982069b81dbc4c09047780d229
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-02-27 09:07:10 +0100

    Finaly

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: A program that generetes a valid key for the program 101-crackme
+ * Return: 0
+ */
+int main(void)
+{
+	const char *password = "MY NAME IS _ACHRAF! AND I'M AN ALX STUDENT!";
+
+	printf("%s", password);
+
+	return 0;
+}
