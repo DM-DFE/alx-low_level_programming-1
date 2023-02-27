7bf45b58ee07b212ea0e3439014891c59382cf5d
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-02-27 06:07:10 +0100

    fix; betty coding style

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
 /**
- * _strcpy - Copies the string pointed to by src, including the terminating null
+ * _strcpy - Copies the string pointed to by src,
+ *			including the terminating null
  * @dest: The destination string
  * @src: The source string
  * Return: The pointer to dest
  */
-char *_strcpy(char *dest, char *src) {
+char *_strcpy(char *dest, char *src)
+{
 	int i;
 
 	for (i = 0; src[i]; i++)
