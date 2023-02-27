7f53a36c5e20fa4feabdde775f6072a18618854f
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-02-27 05:35:04 +0100

    EDIT: Adding more tests to task 1!

--- 0x05-pointers_arrays_strings/test_files/1-main.c
+++ 0x05-pointers_arrays_strings/test_files/1-main.c
@@ -11,10 +11,21 @@ int main(void)
 
 	a = 98;
 	b = 42;
-
 	printf("a=%d, b=%d\n", a, b);
 	swap_int(&a, &b);
 	printf("a=%d, b=%d\n", a, b);
+
+	a = 1024;
+	b = -4096;
+	printf("%d, %d\n", a, b);
+	swap_int(&a, &b);
+	printf("%d, %d\n", a, b);
+
+	a = INT_MAX;
+	b = INT_MIN;
+	printf("%d, %d\n", a, b);
+	swap_int(&a, &b);
+	printf("%d, %d\n", a, b);
 
 	return (0);
 }
