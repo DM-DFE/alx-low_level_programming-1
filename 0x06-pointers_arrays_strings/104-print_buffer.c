27ce574185675bc4e68c8b4001a2af7eaff97205
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-03-01 18:30:55 +0100

    TEST: protecting the function from null input

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -9,18 +9,22 @@ void print_buffer(char *b, int size)
 {
 	int offset, byte_index, char_index;
 
+	if (b == NULL)
+		return;
+
 	for (offset = 0; offset < size; offset += 10)
 	{
 		printf("%08x: ", offset);
 		for (byte_index = 0; byte_index < 10; byte_index++)
 		{
-			(offset + byte_index < size) ? printf("%02x", b[offset + byte_index]) : printf("  ");
+			(offset + byte_index < size) ? printf("%02x", b[offset + byte_index])
+				: printf("  ");
 			(byte_index % 2) ? printf(" ") : 0;
 		}
 		printf(" ");
 		for (char_index = 0; char_index < 10; char_index++)
-			(offset + char_index < size) ? printf("%c", (b[offset + char_index] >= 32 && b[offset + char_index] <= 126) ?
-				b[offset + char_index] : '.') : 0;
+			(offset + char_index < size) ? printf("%c", (b[offset + char_index] >= 32 &&
+				b[offset + char_index] <= 126) ? b[offset + char_index] : '.') : 0;
 		printf("\n");
 	}
 	(size <= 0) ? printf("\n") : 0;
