c75556e568458f8ad0b8c72bc4823c88f432aca7
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-03-01 20:08:17 +0100

    EDIT: reforming code and removing extra space

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,30 +1,71 @@
 #include "main.h"
 
 /**
+ * print_offset - prints the offset of a buffer
+ * @offset: the offset of the memory to print
+ */
+void print_offset(int offset)
+{
+	printf("%08x: ", offset);
+}
+
+/**
+ * print_hex - prints the hex of a buffer
+ * @b: the address of the memory to print
+ * @size: the size of the memory to print
+ * @offset: the offset of the memory to print
+ */
+void print_hex(char *b, const int *size, const int *offset)
+{
+	int byte_index;
+
+	for (byte_index = 0; byte_index < 10; byte_index++)
+	{
+		(*offset + byte_index < *size) ? printf("%02x", b[*offset + byte_index])
+									 : printf("  ");
+		(byte_index % 2) ? printf(" ") : 0;
+	}
+}
+
+/**
+ * print_content - prints the content of a buffer
+ * @b: the address of the memory to print
+ * @size: the size of the memory to print
+ * @offset: the offset of the memory to print
+ */
+void print_content(char *b, const int *size, const int *offset)
+{
+	int char_index;
+
+	for (char_index = 0; char_index < 10; char_index++)
+	{
+		if (*offset + char_index < *size)
+		{
+			if (b[*offset + char_index] >= 32 && b[*offset + char_index] <= 126)
+				printf("%c", b[*offset + char_index]);
+			else
+				printf(".");
+		}
+	}
+}
+
+/**
 * print_buffer - prints a buffer
  * @b: the address of the memory to print
  * @size: the size of the memory to print
  */
 void print_buffer(char *b, int size)
 {
-	int offset, byte_index, char_index;
+	int offset;
 
 	if (b == NULL)
 		return;
 
 	for (offset = 0; offset < size; offset += 10)
 	{
-		printf("%08x: ", offset);
-		for (byte_index = 0; byte_index < 10; byte_index++)
-		{
-			(offset + byte_index < size) ? printf("%02x", b[offset + byte_index])
-				: printf("  ");
-			(byte_index % 2) ? printf(" ") : 0;
-		}
-		printf(" ");
-		for (char_index = 0; char_index < 10; char_index++)
-			(offset + char_index < size) ? printf("%c", (b[offset + char_index] >= 32 &&
-				b[offset + char_index] <= 126) ? b[offset + char_index] : '.') : 0;
+		print_offset(offset);
+		print_hex(b, &size, &offset);
+		print_content(b, &size, &offset);
 		printf("\n");
 	}
 	(size <= 0) ? printf("\n") : 0;
