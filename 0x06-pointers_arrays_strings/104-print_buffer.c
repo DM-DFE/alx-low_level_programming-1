6fd091e267a1811b183614042631cf360bde9a64
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-03-01 18:26:49 +0100

    TEST

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -14,27 +14,14 @@ void print_buffer(char *b, int size)
 		printf("%08x: ", offset);
 		for (byte_index = 0; byte_index < 10; byte_index++)
 		{
-			if (offset + byte_index >= size)
-				printf("  ");
-			else
-				printf("%02x", b[offset + byte_index]);
-			if (byte_index % 2 && byte_index)
-				printf(" ");
+			(offset + byte_index < size) ? printf("%02x", b[offset + byte_index]) : printf("  ");
+			(byte_index % 2) ? printf(" ") : 0;
 		}
+		printf(" ");
 		for (char_index = 0; char_index < 10; char_index++)
-		{
-			if (offset + char_index < size)
-			{
-				if (b[offset + char_index] >= 32 && b[offset + char_index] <= 126)
-					printf("%c", b[offset + char_index]);
-				else
-					printf(".");
-			}
-			if (offset >= size)
-				continue;
-			printf("\n");
-		}
-	}
-	if (size <= 0)
+			(offset + char_index < size) ? printf("%c", (b[offset + char_index] >= 32 && b[offset + char_index] <= 126) ?
+				b[offset + char_index] : '.') : 0;
 		printf("\n");
+	}
+	(size <= 0) ? printf("\n") : 0;
 }
