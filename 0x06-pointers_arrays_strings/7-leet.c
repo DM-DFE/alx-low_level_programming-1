2c4542f62ba67c310d2eba0cfeef4336f36e5299
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-03-01 17:05:43 +0100

    EDIT: small changes

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,6 +1,16 @@
 #include "main.h"
 
 /**
+ * _toupper - converts a lowercase letter to uppercase
+ * @c: character to convert
+ * Return: uppercase character
+ */
+static int _toupper(int c)
+{
+	return ((c >= 'a' && c <= 'z') ? c - 32 : c);
+}
+
+/**
  * leet - encodes a string into 1337
  * @s: string to encode
  * Return: pointer to encoded string
@@ -8,12 +18,12 @@
 char *leet(char *s)
 {
 	int i, j;
-	char *decoding = "aAeEoOtTlL";
-	char *encoding = "4433007711";
+	char *letters = "AEOTL";
+	char *leet = "43071";
 
 	for (i = 0; s[i]; i++)
-		for (j = 0; decoding[j]; j++)
-			(s[i] == decoding[j]) ? s[i] = encoding[j] : s[i];
+		for (j = 0; letters[j]; j++)
+			(_toupper(s[i]) == letters[j]) ? s[i] = leet[j] : s[i];
 
 	return (s);
 }
