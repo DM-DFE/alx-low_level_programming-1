50153a0272ca06248b4a9edf97d9843c820f2e61
Achraf EL Khnissi <40025388+achrafelkhnissi@users.noreply.github.com>
2023-03-16 10:39:07 +0100

    fix: mul by 0

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -29,16 +29,17 @@ void print_result(int *result, int len)
 	int i;
 
 	/* remove leading zeros */
-	for (i = 0; i <= len && result[i] == 0; i++)
+	for (i = 0; i < len && result[i] == 0; i++)
 		;
 
 	/* if all digits are 0, print 0 */
 	if (i == len)
 		printf("0");
 
-	/* print result */
-	for (i = 1; i < len; i++)
+	/* print the result */
+	for (; i < len; i++)
 		printf("%d", result[i]);
+
 	printf("\n");
 }
 
