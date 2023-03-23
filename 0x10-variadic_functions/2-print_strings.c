406065abe894eb99a5ba83bcb398435782385c79
Achraf EL Khnissi <40025388+achrafelkhnissi@users.noreply.github.com>
2023-03-23 09:24:34 +0000

    fix: new line missing

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,5 +19,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		printf("%s%s", str == NULL ? "(nil)" : str,
 			   (i < n - 1 && separator != NULL) ? separator : ""));
 	}
+	printf("\n");
 	va_end(ap);
 }
