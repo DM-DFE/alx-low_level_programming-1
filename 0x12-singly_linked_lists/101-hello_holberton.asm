64732bb573e7bbfe8afe93a2b5c6c2c79eb9791b
Achraf EL Khnissi <40025388+achrafelkhnissi@users.noreply.github.com>
2023-03-30 15:18:16 +0000

    add: task 6\

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,24 +1,24 @@
-; Declare needed C  functions
-	extern	printf		; Declare the C function printf
+; Declare needed C functions
+extern printf
 
-	section .data		; Data section, initialized variables
-msg:
-    db "Hello, Holberton", 0 ; Define the string variable named msg
-fmt:
-    db "%s", 10, 0		; Define the string variable named fmt and initialize it with “%s\n\0”
+section .data
+    msg db "Hello, Holberton", 0 ; Define the string variable named msg
+    fmt db "%s\n", 0 ; Define the string variable named fmt and initialize it with “%s\n\0”
 
-	section .text		; Code section.
+section .text
+global main
+main:
+    push rbp ; set up stack frame
 
-	global main		; the standard gcc entry point
-main:				; the program label for the entry point
-	push	rbp		; set up stack frame
+    ; Pass the arguments to printf
+    mov rdi, fmt ; First argument: the format string
+    mov rsi, msg ; Second argument: the string to print
 
-    mov	rdi,fmt		; Move the address of fmt into rdi register
-    mov	rsi,msg		; Move the address of msg into rsi register
-    mov	rax,0		; Move 0 into rax register
-    call	printf		; Call printf function
+    ; Call printf
+    xor eax, eax ; Clear the EAX register
+    call printf ; Call printf function
 
-	pop	rbp		; restore stack
-
-	mov	rax,0		; normal, no error, return value
-	ret			; return
+    ; Clean up the stack and return
+    pop rbp ; restore stack
+    xor eax, eax ; Clear the EAX register again
+    ret ; return
