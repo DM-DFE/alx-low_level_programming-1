af500c2e5be30915faa7a4bdf3817347d1eec9c9
Achraf EL Khnissi <40025388+achrafelkhnissi@users.noreply.github.com>
2023-04-03 17:26:29 +0000

    add: task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -8,11 +8,19 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *last;
+	listint_t *new, *last;
 
-	last = malloc(sizeof(listint_t));
-	if (last == NULL)
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
 		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	last = *head;
 	while (last->next)
 		last = last->next;
 	last->next = new;
