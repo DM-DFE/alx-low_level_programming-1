aaa2a0c6a81609fa096abcbb7b67912d5df56a97
Achraf EL Khnissi <40025388+achrafelkhnissi@users.noreply.github.com>
2023-04-04 22:30:28 +0000

    test

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -5,7 +5,8 @@
  * @head: pointer to the head of the list
  * Return: the number of nodes in the list
  */
-size_t print_listint_safe(const listint_t *head) {
+size_t print_listint_safe(const listint_t *head)
+{
 	size_t i = 0;
 	const listint_t *tmp;
 
