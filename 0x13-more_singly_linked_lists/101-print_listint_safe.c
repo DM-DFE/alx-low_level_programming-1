02b2c0521897439b303855ac32fc4c8c1a39f963
Achraf EL Khnissi <40025388+achrafelkhnissi@users.noreply.github.com>
2023-04-04 22:28:51 +0000

    test

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -13,12 +13,11 @@ size_t print_listint_safe(const listint_t *head)
 	if (head == NULL)
 		exit(98);
 
-	for (i = 0; head;)
+	for (i = 0; head; i++)
 	{
 		printf("[%p] %d\n", (void *)head, head->n);
 		tmp = head;
 		head = head->next;
-		i++;
 		if (tmp <= head)
 		{
 			printf("-> [%p] %d\n", (void *)head, head->n);
